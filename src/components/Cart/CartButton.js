cf6b3b6d6b393711b64cffce5fd181a02cf924ab
silver1124 <yashtiwari1124>
2023-09-14 15:55:48 +0530

    Shoping page app using reducers

---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,21 @@
+import { uiActions } from '../../store/ui-slice';
+import { useDispatch } from 'react-redux';
+import classes from './CartButton.module.css';
+
+const CartButton = (props) => {
+
+  const dispatch = useDispatch();
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle())
+  }
+
+  return (
+    <button onClick={toggleCartHandler} className={classes.button}>
+      <span>My Cart</span>
+      <span className={classes.badge}>1</span>
+    </button>
+  );
+};
+
+export default CartButton;
